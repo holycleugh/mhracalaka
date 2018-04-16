c203cf924e6cdbd8cb8c865d88c2c36d46e51977
Caolán <kllwynog@gmail.com>
2018-04-15 23:15:50 -0700

    salon final final

---
+++ js/date.js
@@ -0,0 +1,24 @@
+digits = ["naught", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+ordinals = ["naughth", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth"];
+months = ["afteryule", "cake", "retha", "easter", "threemilk", "midsummer", "afterlitha", "grass", "harvest", "winter", "blood", "yuletide"];
+
+date = document.getElementById("date");
+today = new Date();
+date.innerHTML = " the " + datify(today.getDate()) + " of " + monthify(today.getMonth());
+
+function datify(d) {
+  if (d < 10) return ordinals[d % 10];
+  else if (d == 10) {
+    return "tenth";
+  } else if (d < 20) {
+    return "onety-" + ordinals[d % 10];
+  } else if (d == 20) {
+    return "twentieth";
+  } else {
+    return "twenty-" + ordinals[d % 10];
+  }
+}
+
+function monthify(m) {
+  return months[m];
+}
